58b294c30ee8cde3d0247012f2fb3160294346e0
Roman Batavi <romanbatavi98@gmail.com>
2023-01-23 14:49:27 +0700

    [ADD]extremous: try adding method for onchange

--- extremous/models/pendataan_warga.py
+++ extremous/models/pendataan_warga.py
@@ -29,11 +29,38 @@ class extremous(models.Model):
     kec_jkt_pusat = fields.Selection([('cempaka_putih', 'Cempaka Putih'),('gambir', 'Gambir'),('johar_baru', 'Johar Baru'),('kemayoran', 'Kemayoran'),('menteng', 'Menteng'),('sawah_besar', 'Sawah Besar'),('senen', 'Senen'),('tanah_abang', 'Tanah Abang')], string='Kecamatan')
     kec_kep_seribu = fields.Selection([('kep_seribu_utara', 'Kepulauan Seribu Utara'),('kep_seribu_selatan', 'Kepulauan Seribu Selatan')], string='Kecamatan')
     
-    # KECAMATAN =======================================================================
-    kel_cakung = fields.Selection([('cakung_barat', 'Cakung Barat'),('cakung_timur', 'Cakung Timur'),('jatinegara', 'Jatinegara'),('penggilingan', 'Penggilingan'),('pulo_gebang', 'Pulo Gebang'),('pulo_gadung', 'Rawa Terate'),('ujung_menteng', 'Ujung Menteng')], string='Kelurahan')
+    # KELURAHAN JAKARTA TIMUR ==========================================================
+    kel_cakung = fields.Selection([('cakung_barat', 'Cakung Barat'),('cakung_timur', 'Cakung Timur'),('jatinegara', 'Jatinegara'),('penggilingan', 'Penggilingan'),('pulo_gebang', 'Pulo Gebang'),('pulogadung', 'Rawa Terate'),('ujung_menteng', 'Ujung Menteng')], string='Kelurahan')
     kel_cipayung = fields.Selection([('bambu_apus', 'Bambu Apus'),('ceger', 'Ceger'),('cilangkap', 'Cilangkap'),('cipayung', 'Cipayung'),('lubang_buaya', 'Lubang Buaya'),('munjul', 'Munjul'),('pondok_ranggon', 'Pondok Ranggon'),('setu', 'Setu')], string='Kelurahan')
     kel_ciracas = fields.Selection([('cibubur', 'Cibubur'),('ciracas', 'Ciracas'),('kelapa_dua_wetan', 'Kelapa Dua Wetan'),('rambutan', 'Rambutan'),('susukan', 'Susukan')], string='Kelurahan')
     kel_duren_sawit = fields.Selection([('duren_sawit', 'Duren Sawit'),('klender', 'Klender'),('malaka_jaya', 'Malaka Jaya'),('malaka_sari', 'Malaka Sari'),('pondok_kopi', 'Pondok Bambu'),('pondok_kelapa', 'Pondok Kelapa'),('pondok_kopi', 'Pondok Kopi')], string='Kelurahan')
     kel_jatinegara = fields.Selection([('bali_mester', 'Bali Mester'),('bidara_cina', 'Bidara Cina'),('cipinang_besar_selatan', 'Cipinang Besar Selatan'),('cipinang_besar_utara', 'Cipinang Besar Utara'),('cipinang_cempedak', 'Cipinang Cempedak'),('cipinang_muara', 'Cipinang Muara'),('kampung_melayu', 'Kampung Melayu'),('rawa_bunga', 'Rawa Bunga')], string='Kelurahan')
     kel_kramat_jati = fields.Selection([('balekambang', 'Balekambang'),('batu_ampar', 'Batu Ampar'),('cawang', 'Cawang'),('cililitan', 'Cililitan'),('dukuh', 'Dukuh'),('kramat_jati', 'Kramat Jati'),('tengah', 'Tengah')], string='Kelurahan')
+    kel_makasar = fields.Selection([('cipinang_melayu', 'Cipinang Melayu'),('halim_perdana_kusuma', 'Halim Perdana Kusuma'),('kebon_pala', 'Kebon Pala'),('makasar', 'Makasar'),('pinang_ranti', 'Pinang Ranti')], string='Kelurahan')
+    kel_matraman = fields.Selection([('kayu_manis', 'Kayu Manis'),('kebon_manggis', 'Kebon Manggis'),('pal_meriam', 'Pal Meriam'),('pisangan_baru', 'Pisangan Baru'),('utan_kayu_selatan', 'Utan Kayu Selatan'),('utan_kayu_utara', 'Utan Kayu Utara')], string='Kelurahan')
+    kel_pasar_rebo = fields.Selection([('baru', 'Baru'),('cijantung', 'Cijantung'),('gedong', 'Gedong'),('kalisari', 'Kalisari'),('pekayon', 'Pekayon')], string='Kelurahan')
+    kel_pulogadung = fields.Selection([('cipinang', 'Cipinang'),('jati', 'Jati'),('jatinegara_kaum', 'Jatinegara Kaum'),('kayu_putih', 'Kayu Putih'),('pisangan_timur', 'Pisangan Timur'),('pulogadung', 'Pulo Gadung'),('rawamangun', 'Rawamangun')], string='Kelurahan')
+
+    # KELURAHAN JAKARTA BARAT ==========================================================
+    @api.onchange('kota_administrasi')
+    def _onchange_kota_administrasi(self):
+        if self.kota_administrasi:
+            self.kec_jkt_timur = False
+            self.kec_jkt_barat = False
+            self.kec_jkt_selatan = False
+            self.kec_jkt_utara = False
+            self.kec_kep_seribu = False
     
+    @api.onchange('kec_jkt_timur')
+    def _onchange_kec_jkt_timur(self):
+        if self.kec_jkt_timur:
+            self.kel_cakung == False
+            self.kel_cipayung == False
+            self.kel_ciracas == False
+            self.kel_duren_sawit == False
+            self.kel_jatinegara == False
+            self.kel_kramat_jati == False
+            self.kel_makasar == False
+            self.kel_matraman == False
+            self.kel_pasar_rebo == False
+            self.kel_pulogadung == False
