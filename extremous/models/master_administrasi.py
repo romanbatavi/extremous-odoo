6055089ed94309f03e9e4f097bda071a8ac2bc0c
Roman Batavi <romanbatavi98@gmail.com>
2023-01-23 14:07:47 +0700

    [ADD]extremous: pendataan warga alamat

--- extremous/models/master_administrasi.py
+++ extremous/models/master_administrasi.py
@@ -4,7 +4,7 @@ class MasterKotaAdministrasi(models.Model):
     _name = 'master.kota.administrasi'
     _description = 'Master Kota Administrasi'
 
-    nama_kota = fields.Char(compute='_compute_nama_kota', string='nama_kota')
+    # nama_kota = fields.Char(compute='_compute_nama_kota', string='nama_kota')
                 
     is_kota = fields.Selection([
         ('timur', 'Jakarta Timur'),
@@ -15,24 +15,25 @@ class MasterKotaAdministrasi(models.Model):
         ('kep_seribu', 'Kep.Seribu')
     ], string='Kota Administrasi')
 
-    @api.depends('is_kota')
-    def _compute_nama_kota(self):
-        for kota in self:
-            if kota.is_kota == 'timur':
-                kota.nama_kota = 'Jakarta Timur'
-            if kota.is_kota == 'barat':
-                kota.nama_kota = 'Jakarta Barat'
-            if kota.is_kota == 'selatan':
-                kota.nama_kota = 'Jakarta Selatan'
-            if kota.is_kota == 'utara':
-                kota.nama_kota = 'Jakarta Utara'
-            if kota.is_kota == 'pusat':
-                kota.nama_kota = 'Jakarta Pusat'
-            if kota.is_kota == 'kep_seribu':
-                kota.nama_kota = 'Kep.Seribu'
+    # @api.depends('is_kota')
+    # def _compute_nama_kota(self):
+    #     for kota in self:
+    #         if kota.is_kota == 'timur':
+    #             kota.nama_kota = 'Jakarta Timur'
+    #         if kota.is_kota == 'barat':
+    #             kota.nama_kota = 'Jakarta Barat'
+    #         if kota.is_kota == 'selatan':
+    #             kota.nama_kota = 'Jakarta Selatan'
+    #         if kota.is_kota == 'utara':
+    #             kota.nama_kota = 'Jakarta Utara'
+    #         if kota.is_kota == 'pusat':
+    #             kota.nama_kota = 'Jakarta Pusat'
+    #         if kota.is_kota == 'kep_seribu':
+    #             kota.nama_kota = 'Kep.Seribu'
 
-    def name_get(self):
-        result = []
-        for rec in self:
-            result.append((rec.id, '%s' % (rec.nama_kota)))    
-        return result
+    # def name_get(self):
+    #     result = []
+    #     for record in self:
+    #         name = record.is_kota.dict(record._fields['is_kota'].selection).get(record.is_kota)
+    #         result.append((record.id, name))
+    #     return result
--- extremous/models/pendataan_warga.py
+++ extremous/models/pendataan_warga.py
@@ -7,7 +7,7 @@ class extremous(models.Model):
     _name = 'data.warga'
     _description = 'Data Warga'
 
-    # DATA DIRI
+    # DATA DIRI =======================================================================
     name = fields.Char('Nama Lengkap')
     nik = fields.Char('NIK')
     tempat_lahir = fields.Char('Tempat Lahir')
@@ -21,7 +21,7 @@ class extremous(models.Model):
     # KOTA ADMINISTRASI ===============================================================
     kota_administrasi = fields.Selection([('jkt_timur', 'Jakarta Timur'),('jkt_barat', 'Jakarta Barat'),('jkt_selatan', 'Jakarta Selatan'),('jkt_utara', 'Jakarta Utara'),('jkt_pusat', 'Jakarta Pusat'),('kep_seribu', 'Kepulauan Seribu'),], string='Kota Administrasi')
 
-    # KECAMATAN ======================================================================
+    # KECAMATAN =======================================================================
     kec_jkt_timur = fields.Selection([('cakung', 'Cakung'),('cipayung', 'Cipayung'),('ciracas', 'Ciracas'),('duren_sawit', 'Duren Sawit'),('jatinegara', 'Jatinegara'),('kramat_jati', 'Kramat Jati'),('makasar', 'Makasar'),('matraman', 'Matraman'),('pasar_rebo', 'Pasar Rebo'),('pulogadung', 'Pulogadung')], string='Kecamatan')
     kec_jkt_barat = fields.Selection([('cengkareng', 'Cengkareng'),('grogol_petamburan', 'Grogol Petamburan'),('taman_sari', 'Taman Sari'),('tambora', 'Tambora'),('kebon_jeruk', 'Kebon Jeruk'),('kalideres', 'Kalideres'),('palmerah', 'palmerah'),('kembangan', 'Kembangan')], string='Kecamatan')
     kec_jkt_selatan = fields.Selection([('cilandak', 'Cilandak'),('jagakarsa', 'Jagakarsa'),('keb_baru', 'Keb. Baru'),('keb_lama', 'Keb. Lama'),('mampang_prap', 'Mampang Prapatan'),('pancoran', 'Pancoran'),('pasar_minggu', 'Pasar Minggu'),('setia_budi', 'Setia Budi'),('tebet', 'Tebet')], string='Kecamatan')
@@ -29,4 +29,67 @@ class extremous(models.Model):
     kec_jkt_pusat = fields.Selection([('cempaka_putih', 'Cempaka Putih'),('gambir', 'Gambir'),('johar_baru', 'Johar Baru'),('kemayoran', 'Kemayoran'),('menteng', 'Menteng'),('sawah_besar', 'Sawah Besar'),('senen', 'Senen'),('tanah_abang', 'Tanah Abang')], string='Kecamatan')
     kec_kep_seribu = fields.Selection([('kep_seribu_utara', 'Kepulauan Seribu Utara'),('kep_seribu_selatan', 'Kepulauan Seribu Selatan')], string='Kecamatan')
     
-    # KECAMATAN ======================================================================
+    # KECAMATAN =======================================================================
+    kel_cakung = fields.Selection([
+('cakung_barat', 'Cakung Barat'),
+('cakung_timur', 'Cakung Timur'),
+('jatinegara', 'Jatinegara'),
+('penggilingan', 'Penggilingan'),
+('pulo_gebang', 'Pulo Gebang'),
+('pulo_gadung', 'Rawa Terate'),
+('ujung_menteng', 'Ujung Menteng')
+    ], string='Kelurahan')
+    kel_cipayung = fields.Selection([
+('bambu_apus', 'Bambu Apus'),
+('ceger', 'Ceger'),
+('cilangkap', 'Cilangkap'),
+('cipayung', 'Cipayung'),
+('lubang_buaya', 'Lubang Buaya'),
+('munjul', 'Munjul'),
+('pondok_ranggon', 'Pondok Ranggon'),
+('setu', 'Setu')
+    ], string='Kelurahan')
+    kel_ciracas = fields.Selection([
+('key', 'Cibubur'),
+('key', 'Ciracas'),
+('key', 'Kelapa Dua Wetan'),
+('key', 'Rambutan'),
+('key', 'Susukan')
+    ], string='Kelurahan')
+    kel_duren_sawit = fields.Selection([
+('key', 'Duren Sawit'),
+('key', 'Klender'),
+('key', 'Malaka Jaya'),
+ ('key', 'Malaka Sari'),
+('key', 'Pondok Bambu'),
+('key', 'Pondok Kelapa'),
+('key', 'Pondok Kopi')
+    ], string='Kelurahan')
+    kel_duren_sawit = fields.Selection([
+        ('key', 'Bali Mester'),
+        ('key', 'Bidara Cina'),
+        ('key', 'Cipinang Besar Selatan'),
+        ('key', 'Cipinang Besar Utara'),
+        ('key', 'Cipinang Cempedak'),
+        ('key', 'Cipinang Muara'),
+        ('key', 'Kampung Melayu'),
+        ('key', 'Rawa Bunga')
+    ], string='Kelurahan')
+    kel_jatinegara = fields.Selection([
+        ('key', 'Duren Sawit'),
+        ('key', 'Klender'),
+        ('key', 'Malaka Jaya'),
+        ('key', 'Malaka Sari'),
+        ('key', 'Pondok Bambu'),
+        ('key', 'Pondok Kelapa'),
+        ('key', 'Pondok Kopi')
+    ], string='Kelurahan')
+    kel_kramat_jati = fields.Selection([
+        ('key', 'Balekambang'),
+        ('key', 'Batu Ampar'),
+        ('key', 'Cawang'),
+        ('key', 'Cililitan'),
+        ('key', 'Dukuh'),
+        ('key', 'Kramat Jati'),
+        ('key', 'Tengah')
+    ], string='Kelurahan')
